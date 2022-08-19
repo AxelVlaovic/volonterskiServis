8a2813010dae74d5520c435058021b07c3912e4e
OSX <osx@OSXs-iMac.local>
2022-08-19 18:49:11 +0200

    ovakoRadi

--- routes/Users_Activities.js
+++ routes/Users_Activities.js
@@ -27,7 +27,7 @@ function authToken(req,res,next){
 
 route.get('/test',(req,res)=>{
 
-    Users_Activities.findAll({include:['users','activities']})
+    Users_Activities.findAll()
         .then(rows => res.json(rows))
         .catch(err => res.status({msg: 'ovo je problem'}));
 
