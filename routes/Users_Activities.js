383235a8b6d20e77652c1fb1cab69da4621b5a78
OSX <osx@OSXs-iMac.local>
2022-08-19 19:40:25 +0200

    messages

--- routes/Users_Activities.js
+++ routes/Users_Activities.js
@@ -27,7 +27,7 @@ function authToken(req,res,next){
 
 route.get('/test',(req,res)=>{
 
-    Users_Activities.findAll({include:['users']})              
+    Users_Activities.findAll()              
     .then(rows => res.json(rows))
     .catch(err => res.status(500));
 
